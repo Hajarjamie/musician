acbd5b18ce433e36e0221f59bd746ddd1ab4f8ac
Hajarjamie <hajarelhusseiny03@gmail.com>
2023-12-15 23:15:41 +0300

    deleted spects, added new folders/2 components (list-detail) under every 'main' component, added the to app.routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -34,8 +34,127 @@ export const routes: Routes = [
                         loadComponent: () => import('./pages/artist/artist-detail/artist-detail.component').then((c) => c.ArtistDetailComponent)
                     }
                 ]
+            },
+
+            {
+                path: 'group',
+                children: [
+                    {
+                        path: 'list',
+                        loadComponent: () => import('./pages/band/group/group-list/group-list.component').then((c) => c.GroupListComponent)
+                    },
+                    {
+                        path: 'new',
+                        loadComponent: () => import('./pages/band/group/group-detail/group-detail.component').then((c) => c.GroupDetailComponent)
+                    },
+                    {
+                        path: ':id',
+                        loadComponent: () => import('./pages/band/group/group-detail/group-detail.component').then((c) => c.GroupDetailComponent)
+                    }
+                ]
+            },
+            
+            {
+                path: 'concerts',
+                children: [
+                    {
+                        path: 'list',
+                        loadComponent: () => import('./pages/concert/concerts/concerts-list/concerts-list.component').then((c) => c.ConcertsListComponent)
+                    },
+                    {
+                        path: 'new',
+                        loadComponent: () => import('./pages/concert/concerts/concerts-detail/concerts-detail.component').then((c) => c.ConcertsDetailComponent)
+                    },
+                    {
+                        path: ':id',
+                        loadComponent: () => import('./pages/concert/concerts/concerts-detail/concerts-detail.component').then((c) => c.ConcertsDetailComponent)
+                    }
+                ]
+            },
+            {
+                path: 'bands',
+                children: [
+                    {
+                        path: 'list',
+                        loadComponent: () => import('./pages/concert-band/bands/bands-list/bands-list.component').then((c) => c.BandsListComponent)
+                    },
+                    {
+                        path: 'new',
+                        loadComponent: () => import('./pages/concert-band/bands/bands-detail/bands-detail.component').then((c) => c.BandsDetailComponent)
+                    },
+                    {
+                        path: ':id',
+                        loadComponent: () => import('./pages/concert-band/bands/bands-detail/bands-detail.component').then((c) => c.BandsDetailComponent)
+                    }
+                ]
+            },
+
+         {  path: 'house',
+                children: [
+                    {
+                        path: 'list',
+                        loadComponent: () => import('./pages/home/house/house-list/house-list.component').then((c) => c.HouseListComponent)
+                    },
+                    {
+                        path: 'new',
+                        loadComponent: () => import('./pages/home/house/house-detail/house-detail.component').then((c) => c.HouseDetailComponent)
+                    },
+                    {
+                        path: ':id',
+                        loadComponent: () => import('./pages/home/house/house-detail/house-detail.component').then((c) => c.HouseDetailComponent)
+                    }
+                ]
+            }, 
+
+            {  path: 'jazz',
+            children: [
+                {
+                    path: 'list',
+                    loadComponent: () => import('./pages/instrument/jazz/jazz-list/jazz-list.component').then((c) => c.JazzListComponent)
+                },
+                {
+                    path: 'new',
+                    loadComponent: () => import('./pages/instrument/jazz/jazz-detail/jazz-detail.component').then((c) => c.JazzDetailComponent)
+                },
+                {
+                    path: ':id',
+                    loadComponent: () => import('./pages/instrument/jazz/jazz-detail/jazz-detail.component').then((c) => c.JazzDetailComponent)
+                }
+            ]
+        }, 
+
+        
+        {  path: 'singer',
+        children: [
+            {
+                path: 'list',
+                loadComponent: () => import('./pages/role/singer/singer-list/singer-list.component').then((c) => c.SingerListComponent)
+            },
+            {
+                path: 'new',
+                loadComponent: () => import('./pages/role/singer/singer-detail/singer-detail.component').then((c) => c.SingerDetailComponent)
+            },
+            {
+                path: ':id',
+                loadComponent: () => import('./pages/role/singer/singer-detail/singer-detail.component').then((c) => c.SingerDetailComponent)
             }
         ]
-    }
+    }, 
+    {  path: 'drums',
+    children: [
+        {
+            path: 'list',
+            loadComponent: () => import('./pages/role-instrument/drums/drums-list/drums-list.component').then((c) => c.DrumsListComponent)
+        },
+        {
+            path: 'new',
+            loadComponent: () => import('./pages/role-instrument/drums/drums-details/drums-details.component').then((c) => c.DrumsDetailsComponent)
+        },
+        {
+            path: ':id',
+            loadComponent: () => import('./pages/role-instrument/drums/drums-details/drums-details.component').then((c) => c.DrumsDetailsComponent)
+        }
+    ]
+}, 
 
-];
+   ] }, ]
